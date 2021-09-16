6f649b184f19c00aecd3bc9a4c8fd8fddf4a581f
codybum <cbumgardner@gmail.com>
2021-09-16 14:04:06 -0400

    update

--- code/scripts/training.py
+++ code/scripts/training.py
@@ -7,8 +7,8 @@ import torch
 from apex import amp
 from py_config_runner.config_utils import TRAINVAL_CONFIG, assert_config, get_params
 from py_config_runner.utils import set_seed
-from utils import exp_tracking
-from utils.handlers import predictions_gt_images_handler
+from code.utils import exp_tracking
+from code.utils.handlers import predictions_gt_images_handler
 
 import ignite
 import ignite.distributed as idist
@@ -16,6 +16,8 @@ from ignite.contrib.engines import common
 from ignite.engine import Engine, Events, _prepare_batch, create_supervised_evaluator
 from ignite.metrics import Accuracy, TopKCategoricalAccuracy
 from ignite.utils import setup_logger
+
+
 
 
 def initialize(config):
